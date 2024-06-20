83b6472462c6c2187735b57c90351b3afce58da6
Faqihaja354 <fmatuzsahro@gmail.com>
2024-06-20 17:23:41 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
 import Image from "next/image";
+
+import {Stack,Button,Card,CardText} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
@@ -6,7 +10,29 @@ export default function Home() {
       <h1 style={{fontSize :"24px", color: "blue"}}>Home Movie List</h1>
       <h2 style={{fontSize :"20px", color: "green"}}>Faqih Matuz Sahro</h2>
       <h3 style={{fontSize :"18px", color: "red"}}>Kelas Pemrograman Web 2024</h3>
-      <h4>kocak lu....</h4>
+      
+    <Stack direction="horizontal" gap={2}>
+      <Button as="a" variant="primary">
+      Button as link
+      </Button>
+    <Button as="a" variant="success">
+      Button as link
+    </Button>
+    </Stack> 
+    <br/>
+    <Card className="bg-transparent text-white text-center movieimage">
+      {/* <Image src="https://cdna.artstation.com/p/assets/images/images/051/931/548/large/aimistics-xmen-tw-min.jpg?1658514664" 
+      alt = "card image" height={300} width={480}></Image> */}
+
+      <img className="card-img-top"
+      src="https://cdna.artstation.com/p/assets/images/images/051/931/548/large/aimistics-xmen-tw-min.jpg?1658514664"
+      alt="card image cap"></img>
+
+      <div className="bg-dark p-1 m-1">
+      <CardText> FILM X-man Apocalypce</CardText>
+      <CardText>Rating 7.9</CardText>
+      </div>
+    </Card>
     </div>
   )
 }
